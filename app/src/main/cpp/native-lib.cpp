4d78f25f03aefad1460d1e4225f150ef91b1b7c9
fanmingyi <635555698@qq.com>
2017-05-05 18:00:11 +0800

    添加多种变声

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,6 +3,106 @@
 #include <iostream>
 #include <fmod.hpp>
 #include <unistd.h>
+//变音
+extern  "C"
+JNIEXPORT void JNICALL
+Java_com_example_fmy_selfplaymusic_MainActivity_effects(JNIEnv *env, jobject instance, jint flag) {
+
+    //音频地址
+  char * rawPath = "file:///android_asset/te.mp3";
+
+    //FMOD系统对象
+    FMOD::System       *system         = 0;
+    //音效
+    FMOD::Sound        *sound          = 0;
+    //声轨
+    FMOD::Channel       *channel       = 0;
+    //声轨集合
+    FMOD::ChannelGroup  * mastergroup  = 0;
+    //一个数字信号处理单元
+    FMOD::DSP            *dsp        = 0;
+
+    //创建对象
+    FMOD::System_Create(&system);
+
+    //初始化 系统对象最大声轨为32
+    system->init(32,FMOD_INIT_NORMAL,0);
+
+    //加载声音 如果过大建议你用FMOD_CREATESTREAM 标志
+    system->createSound(rawPath,FMOD_DEFAULT,0,&sound);
+
+    //得到声音轨道集合
+    system->getMasterChannelGroup(&mastergroup);
+
+    //播放音乐
+    system->playSound(sound,0,false,&channel);
+
+    switch (flag){
+        //萝莉
+        case 1:
+            //创建音频处理DSP 类型 为音频提高
+            system->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT,&dsp);
+           //设置dsp大小
+            dsp->setParameterFloat(FMOD_DSP_PITCHSHIFT_PITCH,2.0);
+
+            //添加一个处理单元到通道
+            channel->addDSP(0,dsp);
+
+            break;
+        //惊悚
+        case 2:
+            //创建音频处理DSP 类型 为颤抖
+            system->createDSPByType(FMOD_DSP_TYPE_TREMOLO,&dsp);
+            //设置低频震旦器频率
+            dsp->setParameterFloat(FMOD_DSP_TREMOLO_FREQUENCY,20);
+            //设置声音歪斜
+            dsp->setParameterFloat(FMOD_DSP_TREMOLO_SKEW,0.5);
+
+            //添加一个处理单元到通道
+            channel->addDSP(0,dsp);
+            break;
+            //大叔
+        case 3:
+
+            //创建音频处理DSP 类型 为音频提高
+            system->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT,&dsp);
+            //设置dsp大小
+            dsp->setParameterFloat(FMOD_DSP_PITCHSHIFT_PITCH,0.5);
+
+            //添加一个处理单元到通道
+            channel->addDSP(0,dsp);
+            break;
+            //搞怪
+        case 4:
+        {
+
+            float mfrequency = 0;
+            //得到频率
+            channel->getFrequency(&mfrequency);
+            channel->setFrequency(mfrequency*2);
+            break;
+        }
+
+            //空灵
+        case 5 :{
+            //创建音频处理DSP 类型 为音频提高
+            system->createDSPByType(FMOD_DSP_TYPE_ECHO,&dsp);
+            //设置dsp大小  延迟声
+            dsp->setParameterFloat(FMOD_DSP_ECHO_DELAY,300);
+            //回音音量
+            dsp->setParameterFloat(FMOD_DSP_ECHO_FEEDBACK,20);
+
+            //添加一个处理单元到通道
+            channel->addDSP(0,dsp);
+            break;
+        }
+
+
+
+    }
+
+}
+
 using namespace std;
 extern  "C"
 ////点击按钮播放音乐
--- app/src/main/java/com/example/fmy/selfplaymusic/MainActivity.java
+++ app/src/main/java/com/example/fmy/selfplaymusic/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     Context mContext;
 
     //要播放的音乐文件地址
-    public static final String  music_path = "file:///android_asset/jaguar.wav";
+    public static final String  music_path = "file:///android_asset/te.mp3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +51,33 @@ public class MainActivity extends AppCompatActivity {
 
         //播放音乐
         playsMusic(music_path);
+
+        }
+
+    public native  void  effects(int flag);
+
+    public void onClick2(View view) {
+
+        TextView t = (TextView) view;
+
+        String flag =t.getText().toString();
+
+        //变音
+        switch (flag) {
+            case "萝莉":
+                effects(1);
+                break;
+            case "惊悚":
+                effects(2);
+            case "大叔":
+                effects(3);
+                break;
+            case "搞怪":
+                effects(4);
+            case "空灵":
+                effects(5);
+                break;
+        }
 
     }
 }
